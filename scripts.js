38a90872c784620978c09de0c04ea5c00056ffd5
linuxfanboy4 <imlinuxfanboy@outlook.com>
2025-04-04 09:32:46 +0600

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    ScrollReveal().reveal('.feature-card', {
+        duration: 1000,
+        distance: '50px',
+        origin: 'bottom',
+        delay: 100,
+        interval: 150
+    });
+});
